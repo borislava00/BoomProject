352a3c65664641cd8bc85331169322545969a3cd
Borislava <bobiivanova00@gmail.com>
2021-10-13 16:25:17 +0300

    add svg

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -6,6 +6,7 @@ import CssBaseline from '@mui/material/CssBaseline';
 import { CacheProvider } from '@emotion/react';
 import theme from '../src/theme';
 import createEmotionCache from '../src/createEmotionCache';
+import Logo from '../src/components/logo/Logo.jsx'
 
 // Client-side cache, shared for the whole session of the user in the browser.
 const clientSideEmotionCache = createEmotionCache();
@@ -24,6 +25,7 @@ export default function MyApp(props) {
         <CssBaseline />
         <Component {...pageProps} />
       </ThemeProvider>
+      <Logo />
     </CacheProvider>
   );
 }
