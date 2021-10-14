0afb2af116efc7fc77ac99c9146acb4fcfe78532
Borislava <bobiivanova00@gmail.com>
2021-10-14 18:08:35 +0300

    separate button in different Grids

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -16,9 +16,13 @@ function Header() {
       <Container className={classNames(styles.container)} maxWidth="xl">
             <Logo />
             <SearchBar />
-            <Grid container spacing={3} sx={{ justifycontent: 'flex-end' , display: 'contents'}}>
+            <Grid item>
                 <Button component={Link} sx={{ borderRadius: 10 , color: 'white' }} noLinkStyle href="/">Home</Button>
+            </Grid>
+            <Grid item>
                 <Button sx={{ color: 'white' , borderRadius: 10 }}>Activity</Button>
+            </Grid>
+            <Grid item>
                 <Button variant="contained" sx={{ borderRadius: 10 , bgcolor: '#4e24f2'}} >Explore</Button>
             </Grid>
       </Container>
