23b632a5c434a4ddda01503960a7ba17f68a79e4
Borislava <bobiivanova00@gmail.com>
2021-10-15 15:45:53 +0300

    change size of container

--- src/components/avatar/Avatar.jsx
+++ src/components/avatar/Avatar.jsx
@@ -6,7 +6,7 @@ function AvatarComponent({ size = 90 , url , verified = false }) {
     return (
         <div className={classNames(styles.wrapper)}>
             <Container className={classNames(styles.container)} size={{ width: size , heigth : size}}>
-                <img className={classNames(styles.image)} src={url} />
+                <img className={classNames(styles.image)} src={url} size={{ width: size , heigth : size}} />
                 {verified ? <img className={classNames(styles.badge)} src={'/images/verified.svg'} /> : null}
             </Container>
         </div>
