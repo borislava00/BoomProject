a5584ab206bc48343ed370b9a384bb328f767f9a
Borislava <bobiivanova00@gmail.com>
2021-10-16 12:57:43 +0300

    make avatar image fill the whole div

--- src/components/avatar/Avatar.jsx
+++ src/components/avatar/Avatar.jsx
@@ -5,7 +5,7 @@ import styles from "./Avatar.module.scss";
 function AvatarComponent({ size = 90 , url , verified = false }) {
     return (
         <div className={classNames(styles.wrapper)}>
-            <Container className={classNames(styles.container)} sx={{ width: 300 , height : 300}}>
+            <Container className={classNames(styles.container)} sx={{ width: size , height : size}}>
                 <img className={classNames(styles.image)} src={url} /> 
                 {verified ? <img className={classNames(styles.badge)} src={'/images/verified.svg'} /> : null}
             </Container>
