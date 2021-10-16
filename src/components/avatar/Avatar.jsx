4fff5e498231f2d0056330aa04f78a6a20d642d2
Borislava <bobiivanova00@gmail.com>
2021-10-16 10:50:36 +0300

    make avatar image fill the whole div

--- src/components/avatar/Avatar.jsx
+++ src/components/avatar/Avatar.jsx
@@ -2,11 +2,11 @@ import { Badge , Container } from '@mui/material';
 import classNames from "classnames";
 import styles from "./Avatar.module.scss";   
 
-function AvatarComponent({ size = 90 , url , verified = false }) {
+function AvatarComponent({ size = '90px' , url , verified = false }) {
     return (
         <div className={classNames(styles.wrapper)}>
-            <Container className={classNames(styles.container)} size={{ width: size , heigth : size}}>
-                <img className={classNames(styles.image)} src={url} size={{ width: size , heigth : size}} />
+            <Container className={classNames(styles.container)} sx={{ width: size , heigth : size}}>
+                <img className={classNames(styles.image)} src={url} />
                 {verified ? <img className={classNames(styles.badge)} src={'/images/verified.svg'} /> : null}
             </Container>
         </div>
