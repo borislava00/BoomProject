c38611cf4582c4367759cad2b1ffcda9be24ca19
Borislava <bobiivanova00@gmail.com>
2021-10-26 14:51:20 +0300

    implement Auction component

--- pages/about/index.jsx
+++ pages/about/index.jsx
@@ -3,6 +3,7 @@ import Container from "@mui/material/Container";
 import Grid from "@mui/material/Grid";
 import Header from "../../src/components/header/Header";
 import Trending from "../../src/components/trending/Trending";
+import Auctions from "../../src/components/auctions/Auctions"
 
 export default function About() {
 
@@ -17,7 +18,6 @@ export default function About() {
         },
         price : '~1',
         currency : 'ETH',
-        timeLeft : 50,
     },
     {
         name : 'Judie',
@@ -54,12 +54,64 @@ export default function About() {
     }
 ]
 
+const liveCards = [
+  {
+      name : 'Ivy',
+      likes : 1100,
+      mediaUrl : 'images/nft.jpg',
+      user : {
+        avatarUrl : 'images/avatar.png',
+        verified : true,
+      },
+      price : '~1',
+      currency : 'ETH',
+      timeLeft : 50,
+  },
+  {
+      name : 'Judie',
+      likes : 1100,
+      mediaUrl : 'images/nft.jpg',
+      user : {
+          avatarUrl : 'images/avatar.png',
+          verified : true,
+      },
+      price : '~2.3',
+      currency : 'ETH',
+      timeLeft : 30,
+  },
+  {
+      name : 'Juniper',
+      likes : 1100,
+      mediaUrl : 'images/nft.jpg',
+      user : {
+          avatarUrl : 'images/avatar.png',
+          verified : true,
+      },
+      price : '5',
+      currency : 'ETH',
+      timeLeft : 60,
+  },
+  {
+      name : 'Maple',
+      likes : 1100,
+      mediaUrl : 'images/nft.jpg',
+      user : {
+          avatarUrl : 'images/avatar.png',
+          verified : true,
+      },
+      price : '10',
+      currency : 'ETH',
+      timeLeft : 20,
+  }
+]
+
   return (
     <Container maxWidth="sm">
       <Grid container justifyContent="center">
         <Grid item>
           <Header/>
           <Trending cards={cards} />
+          <Auctions cards={liveCards} />
         </Grid>
       </Grid>
     </Container>
---
+++ src/components/auctions/Auctions.jsx
@@ -0,0 +1,45 @@
+import { Box , Container , Grid , Select , MenuItem , FormControl } from "@mui/material"
+import Card from "../card/Card";
+import { useState } from "react";
+import styles from './Auctions.module.scss';
+
+function Auctions({ cards = [] }) {
+    const [time, setTime] = useState('');
+
+    const handleChange = (event) => {
+        setTime(event.target.value);
+    }
+
+    return (
+        <div className={styles.wrapper}>
+            <Container className={styles.container} maxWidth="xl">
+                <Grid container className={styles.gridContainer}>
+                    <Grid item className={styles.titleWrapper}>
+                        <div className={styles.title} >Live Auctions</div>
+                    </Grid>
+                    <Grid item className={styles.selectWrapper}>
+                        <Select className={styles.select}
+                        value={time}
+                        onChange={handleChange}
+                        displayEmpty
+                        inputProps={{ 'aria-label': 'Without label' }}
+                        >
+                            <MenuItem value="">This week</MenuItem>
+                            <MenuItem value={10}>Today</MenuItem>
+                            <MenuItem value={20}>This month</MenuItem>
+                            <MenuItem value={30}>This year</MenuItem>
+                        </Select>
+                    </Grid>
+                    <Grid item className={styles.cards} xl>
+                        <Card key={0} {...cards[0]} />
+                        <Card key={1} {...cards[1]} />
+                        <Card key={2} {...cards[2]} />
+                        <Card key={3} {...cards[3]} />
+                    </Grid>
+                </Grid>
+            </Container>
+        </div>
+    );
+}
+
+export default Auctions;
