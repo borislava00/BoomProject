b7e95871cbe358d734a19a026cdc84c4e4c5f01a
Borislava <bobiivanova00@gmail.com>
2021-10-27 14:25:10 +0300

    add src to img

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -13,7 +13,7 @@ function Featured({ items = [] }) {
                     cols={4}
                     rowHeight={121}
                     >
-                    {/* {items.map((item , index ) => (
+                    {items.map((item , index ) => (
                         <ImageListItem key={index} cols={item.cols || 1} rows={item.rows || 1}>
                             <img
                                 {...srcset(item.img, 121, item.rows, item.cols)}
@@ -22,13 +22,7 @@ function Featured({ items = [] }) {
                                 onClick={() => router.push(item.href)}
                             />
                         </ImageListItem>
-                    ))} */}
-                    {items ?
-                        <ImageListItem>
-                        <img src='https://images.unsplash.com/photo-1551963831-b3b1ca40c98e?w=500&h=500'
-                            onClick={() => router.push('https://images.unsplash.com/photo-1551963831-b3b1ca40c98e?w=500&h=500')} />
-                    </ImageListItem>  : null }
-            
+                    ))} 
                 </ImageList>
             </Container>
         </div>
