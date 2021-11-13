3cd81e9a8d8f689739404ab37a9ddbb390a9bad1
Borislava <bobiivanova00@gmail.com>
2021-11-13 13:06:39 +0200

    add productInfoTitle component

--- pages/index.jsx
+++ pages/index.jsx
@@ -11,6 +11,7 @@ import How from "../src/components/how/How";
 import Featured from "../src/components/featured/Featured";
 import TopCollectors from "../src/components/collectors/TopCollectors";
 import ProductImage from "../src/components/product/ProductImage";
+import ProductInfoTitle from "../src/components/product/ProductInfoTitle";
 
 export default function Index() {
   const [ featuredCrads , setFeaturedCards ] = useState([]);
@@ -41,7 +42,8 @@ export default function Index() {
       <TopCollectors collectors={topCollectors.collectors} />
       <How {...howSteps.how} />
       <Auctions cards={nfts.liveCards} />
-      <ProductImage url={"/images/nft.jpg"} />
+      <ProductImage url={'https://s3-alpha-sig.figma.com/img/5700/961d/873ed585dda6af33c56c3e41f33c9c2b?Expires=1637539200&Signature=Drw0e4a2DuK5AD8r3gjLMSdfSnIkfaxKXqvZO908LZ6oL-SzEwOjRNpoC6xzgOmiJcECIpWIdrZBygVydLzx2JImqNFfVZexlhG600x1lCGT9LsSNaNZHu4lWIIy1zSrmCB-jD0TLfHX2RpRYQITU7R~dvJJ5ZkKkVpnRJy8IHg3o0GHiDjlYD3ycN-Jv4EIdlaIGVjSo0uPRLxHMSjtG3v~ob4WOpt2Gnt0MRA3WlRuW96RpziBc9xA6jMyR-Kbl0keLwCzyQD~5-Kuu~gJoTYOyOHw30nK-oKecBTbBQ617nvlx4pIV~Hhpwze37Nosj4Qq4UGcWza4YsNAn9brQ__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA'} />
+      <ProductInfoTitle text={'Shallow Son'} />
       <Footer />
     </div>
   );
