443e79c4de5590c1b8ffad4da4bc43bbbd231570
Borislava <bobiivanova00@gmail.com>
2021-11-16 16:34:49 +0200

    add stats class

--- src/components/product/ProductInfo.jsx
+++ src/components/product/ProductInfo.jsx
@@ -12,17 +12,17 @@ export default function ProductInfo({ title = " " , creator = " " , price = 0 , 
     return (
         <div className={styles.wrapper}>
             <div className={styles["product-info"]}>
-                <ProductInfoTitle text={title} />
-                <Stack spacing={3}>
+                <Stack className={styles.stats} spacing={3}>
+                    <ProductInfoTitle text={title} />
                     <ProductInfoPrice amount={price} currency={currency} />
                     {isLive && <ProductInfoStatus />}
                     <ProductInfoLikes amount={likes} />
                 </Stack>
                 <Grid container spacing={2}>
-                    <Grid item xs={6} md={8}>
-                        <ProductInfoCreator name={'toadnft'} avatar={'https://s3-alpha-sig.figma.com/img/e8c9/9cd5/9b8596b5f1e73a55d94965e77c2e61cd?Expires=1637539200&Signature=O6ml7MEWv1NtU7Qu498ujyPukHYNd9RRyLSPgz4V0mmHlWAxymkqG2utJp0PW~FaEe0yGbeaj37cz9FuRAUHHzPNzPvV8Ym8IK2v19N-zhAabC5qtEQ6lAsH6QBmdKbPQJLwN9QaJIpquUtWfwZ10Lvlf3XNUUrcmDbK41lfFwe~pQKrelvaM2Fft3FYNP-PJP7P8CUzIoOqmkV9Dk7XUFcMoLN~ZjorzSzh-M-LengvgbNkuzSwEwVH4iZICx4rAu83mCRnOeNGPvZyq5rC6O19m5FxLxdZg-qMhlvz9xVMTIsa4FMYCpMaJmpIE5rIvCl5YnL1QNcEmPcIOAO2-w__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA'} />
+                    <Grid item xs={6} md={5}>
+                        <ProductInfoCreator {...creator} />
                     </Grid>
-                    <Grid item xs={6} md={4}>
+                    <Grid item xs={7} md={5}>
                         <ProductInfoTimer timeEnd={onTimeEnd} onTimeEnd={onTimeEnd} />
                     </Grid>
                 </Grid>
