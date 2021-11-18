4def309547fb4b94052c181abdd3bbd0f3fc60c0
Borislava <bobiivanova00@gmail.com>
2021-11-18 20:51:22 +0200

    add table-row class

--- src/components/product/ProductTabs.jsx
+++ src/components/product/ProductTabs.jsx
@@ -1,17 +1,42 @@
 import styles from "./ProductTabs.module.scss"
 import User from "../user/User";
 import { useState } from "react";
+import PropTypes from 'prop-types';
 import { formatDistance, parseISO } from 'date-fns'
-import {  Tab , Table , TableBody , TableRow , TableCell } from "@mui/material";
-import { TabContext , TabList , TabPanel } from '@mui/lab';
+import { Box , Typography , Tabs , Tab , Table , TableBody , TableRow , TableCell } from "@mui/material";
 
+function TabContext(props) {
+    const { children, value, index, ...other } = props;
+  
+    return (
+      <div
+        role="tabpanel"
+        hidden={value !== index}
+        id={`simple-tabpanel-${index}`}
+        aria-labelledby={`simple-tab-${index}`}
+        {...other}
+      >
+        {value === index && (
+          <Box sx={{ p: 3 }}>
+            <Typography component={'span'}>{children}</Typography>
+          </Box>
+        )}
+      </div>
+    );
+  }
+  
+  TabContext.propTypes = {
+    children: PropTypes.node,
+    index: PropTypes.number.isRequired,
+    value: PropTypes.number.isRequired,
+  };
+  
 
 export default function ProductTabs ({ text = " " , bids = [ ] }) {
+    const [value, setValue] = useState(0);
 
-    const [value, setValue] = useState('1');
-  
     const handleChange = (event, newValue) => {
-      setValue(newValue);
+        setValue(newValue);
     };
 
     const setOpacity = (index) => {
@@ -20,33 +45,33 @@ export default function ProductTabs ({ text = " " , bids = [ ] }) {
         }
         return 'rgba(78,36,242, 0.15)'
     }
-  
-    return (
-      <div className={styles["product-tabs"]}>
-        <TabContext value={value}>
-        <div className={styles.tabs} sx={{ borderBottom: 1, borderColor: 'divider' }}>
-            <TabList onChange={handleChange} aria-label="lab API tabs example">
-                <Tab className={styles["tab-details"]} label="DETAILS" value="1"/>
-                <Tab className={styles["tab-bids"]} label="BIDS" value="2"/>
-            </TabList>
+
+    return ( 
+        <div className={styles["product-tabs"]}>
+            <div className={styles.tabs}>
+                <Tabs value={value} onChange={handleChange} >
+                    <Tab className={styles["tab-details"]} label="DETAILS" />
+                    <Tab className={styles["tab-bids"]} label="BIDS" />
+                </Tabs>
+            </div>
+            <TabContext value={value} index={0}>
+                {text}
+            </TabContext>
+            <TabContext value={value} index={1}>
+                <Table>
+                    <TableBody>
+                        {bids.map( ( bid , index ) => ( 
+                            <TableRow className={styles["table-row"], `table-row-${index}`} key={index} sx={{ backgroundColor : setOpacity(index) }}>
+                                <TableCell>
+                                    <User {...bid.user} />
+                                </TableCell>
+                                <TableCell>{bid.amount} ETH</TableCell>
+                                <TableCell>{formatDistance(parseISO(bid.date), new Date(), { addSuffix: true })}</TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>               
+            </TabContext>    
         </div>
-        <TabPanel value="1">{text}</TabPanel>
-        <TabPanel value="2">
-            <Table>
-                <TableBody>
-                    {bids.map( ( bid , index ) => ( 
-                        <TableRow className={styles["table-row"], `table-row-${index}`} key={index} sx={{ backgroundColor : setOpacity(index) }}>
-                            <TableCell>
-                                <User {...bid.user} />
-                            </TableCell>
-                            <TableCell>{bid.amount} ETH</TableCell>
-                            <TableCell>{formatDistance(parseISO(bid.date), new Date(), { addSuffix: true })}</TableCell>
-                        </TableRow>
-                    ))}
-                </TableBody>
-            </Table>
-        </TabPanel>
-        </TabContext>
-      </div>
     );
 }
