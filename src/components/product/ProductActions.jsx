4d036c6f7ba5b0773597775545c305e6187ce682
Borislava <bobiivanova00@gmail.com>
2021-11-23 18:00:41 +0200

    change className of the roor element

--- src/components/product/ProductActions.jsx
+++ src/components/product/ProductActions.jsx
@@ -1,13 +1,21 @@
 import styles from "./ProductActions.module.scss"
 import { Button , Grid } from "@mui/material";
 
-export default function ProductActions({ isLive = false , currency = " " , buyAmount = 0 , bidAmount = 0 , onBuy , onBid }) {
+const hanlderOnBuy = () => {
+
+}
+
+const hanlderOnBid = () => {
+
+}
+
+export default function ProductActions({ isLive = false , currency = " " , buyAmount = 0 , bidAmount = 0 , onBuy = hanlderOnBuy , onBid = hanlderOnBid }) {
     return(
-        <div className={styles["product-actions"]}>
-            <Grid container spacing={2}>
-                <Grid item xs={6} md={8}>
-                    <Button disabled={!isLive} className={styles.button} variant="contained" onClick={onBuy}>Buy for {buyAmount} {currency}</Button>
-                    <Button disabled={!isLive} className={styles.button , styles.bidButton} variant="outlined" color="success" onClick={onBid}>Place a bid for {bidAmount} {currency}</Button>
+        <div className={styles["product-action"]}>
+            <Grid container spacing={5}>
+                <Grid item xs={7} md={5}>
+                    <Button disabled={!isLive} className={styles.button} variant="contained" onClick={ onBuy }>Buy for {buyAmount} {currency}</Button>
+                    <Button disabled={!isLive} className={styles.button , styles.bidButton} variant="outlined" color="success" onClick={ onBid }>Place a bid for {bidAmount} {currency}</Button>
                 </Grid>
             </Grid>
         </div>
