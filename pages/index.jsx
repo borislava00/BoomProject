586439477710dcce338cb90ce94e721d93a3da14
Borislava <bobiivanova00@gmail.com>
2021-11-23 20:08:42 +0200

    add every product component into ProductContainer component

--- pages/index.jsx
+++ pages/index.jsx
@@ -10,16 +10,7 @@ import Footer from "../src/components/footer/Footer";
 import How from "../src/components/how/How";
 import Featured from "../src/components/featured/Featured";
 import TopCollectors from "../src/components/collectors/TopCollectors";
-import ProductImage from "../src/components/product/ProductImage";
-import ProductInfoTitle from "../src/components/product/ProductInfoTitle";
-import ProductInfoPrice from "../src/components/product/ProductInfoPrice";
-import ProductInfoStatus from "../src/components/product/ProductInfoStatus";
-import ProductInfoLikes from "../src/components/product/ProductInfoLikes";
-import ProductInfoCreator from "../src/components/product/ProductInfoCreator";
-import ProductInfoTimer from "../src/components/product/ProductInfoTimer";
-import ProductInfo from "../src/components/product/ProductInfo";
-import ProductTabs from "../src/components/product/ProductTabs";
-import ProductActions from "../src/components/product/ProductActions";
+import ProductContainer from "../src/components/product/ProductContainer";
 
 export default function Index() {
   const [ featuredCrads , setFeaturedCards ] = useState([]);
@@ -50,16 +41,7 @@ export default function Index() {
       <TopCollectors collectors={topCollectors.collectors} />
       <How {...howSteps.how} />
       <Auctions cards={nfts.liveCards} />
-      <ProductImage url={'https://s3-alpha-sig.figma.com/img/5700/961d/873ed585dda6af33c56c3e41f33c9c2b?Expires=1637539200&Signature=Drw0e4a2DuK5AD8r3gjLMSdfSnIkfaxKXqvZO908LZ6oL-SzEwOjRNpoC6xzgOmiJcECIpWIdrZBygVydLzx2JImqNFfVZexlhG600x1lCGT9LsSNaNZHu4lWIIy1zSrmCB-jD0TLfHX2RpRYQITU7R~dvJJ5ZkKkVpnRJy8IHg3o0GHiDjlYD3ycN-Jv4EIdlaIGVjSo0uPRLxHMSjtG3v~ob4WOpt2Gnt0MRA3WlRuW96RpziBc9xA6jMyR-Kbl0keLwCzyQD~5-Kuu~gJoTYOyOHw30nK-oKecBTbBQ617nvlx4pIV~Hhpwze37Nosj4Qq4UGcWza4YsNAn9brQ__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA'} />
-      <ProductInfoTitle text={'Shallow Son'} />
-      <ProductInfoPrice amount={3} currency={'ETH'} />
-      <ProductInfoStatus />
-      <ProductInfoLikes amount={1100} />
-      <ProductInfoCreator name={'toadnft'} avatar={'https://s3-alpha-sig.figma.com/img/e8c9/9cd5/9b8596b5f1e73a55d94965e77c2e61cd?Expires=1637539200&Signature=O6ml7MEWv1NtU7Qu498ujyPukHYNd9RRyLSPgz4V0mmHlWAxymkqG2utJp0PW~FaEe0yGbeaj37cz9FuRAUHHzPNzPvV8Ym8IK2v19N-zhAabC5qtEQ6lAsH6QBmdKbPQJLwN9QaJIpquUtWfwZ10Lvlf3XNUUrcmDbK41lfFwe~pQKrelvaM2Fft3FYNP-PJP7P8CUzIoOqmkV9Dk7XUFcMoLN~ZjorzSzh-M-LengvgbNkuzSwEwVH4iZICx4rAu83mCRnOeNGPvZyq5rC6O19m5FxLxdZg-qMhlvz9xVMTIsa4FMYCpMaJmpIE5rIvCl5YnL1QNcEmPcIOAO2-w__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA'} />
-      <ProductInfoTimer timeEnd={0.00005} />
-      <ProductInfo title={"John"} creator={{"name":"George","avatar":"https://nft-auction.herokuapp.com/uploads/0xa6dbe6b4f8e2905c26e123ec6fd08a8f7200dbc1_64120a76f4.jpg","verified":true}} price={20} currency={"ETH"} likes={20} timeEnd={"2022-09-02T20:43:19.149Z"} isLive={true} />
-      <ProductTabs text={"i m dsnfisdv"} bids={[{"user":{"avatar":"/images/avatar.png","name":"hrisi","verified":true},"amount":30,"date":"2021-10-22T08:29:23.382Z"},{"user":{"avatar":"/images/avatar.png","name":"maxi","verified":true},"amount":1000,"date":"2021-10-22T08:29:23.382Z"}]} />
-      <ProductActions isLive={true} currency={"ETH"} buyAmount={50} bidAmount={9} />
+      <ProductContainer name={"Ergonomic Concrete Tuna"} owner={{"username":"Justen_King18","verified":true,"avatar":{"url":"https://nft-auction.herokuapp.com/uploads/thumbnail_0x7d9debcf75a71bbb5c533804c9845d313fe3f6aa_ec98dd79b9.jpg"}}} price={20} currency={"ETH"} likes={25} auction_end={"2022-09-02T20:43:19.149Z"} details={"asdasdads"} source={{"url":"https://nft-auction.herokuapp.com/uploads/thumbnail_0x7d9debcf75a71bbb5c533804c9845d313fe3f6aa_ec98dd79b9.jpg"}} bids={[{"user":{"info":20,"name":"John","verified":true,"avatar":"https://nft-auction.herokuapp.com/uploads/thumbnail_0xa6dbe6b4f8e2905c26e123ec6fd08a8f7200dbc1_64120a76f4.jpg"},"date":"2021-10-22T08:29:23.382Z","amount":20}]} />
       <Footer />
     </div>
   );
---
+++ src/components/product/ProductContainer.jsx
@@ -0,0 +1,23 @@
+import styles from "./ProductContainer.module.scss";
+import ProductImage from "./ProductImage";
+import ProductInfo from "./ProductInfo";
+import ProductTabs from "./ProductTabs";
+import ProductActions from "./ProductActions";
+import { Grid } from "@mui/material";
+
+export default function ProductContainer({ name , owner , price , currency , likes , auction_end , details , bids , source }) {
+    return (
+        <div className={styles["product-container"]}>
+            <Grid container spacing={2}>
+                    <Grid item xs={6} md={5}>
+                        <ProductImage url={owner.avatar.url} />
+                    </Grid>
+                    <Grid item xs={5} md={7}>
+                        <ProductInfo title={name} creator={owner} price={price} currency={currency} likes={likes} timeEnd={auction_end} />
+                        <ProductTabs text={details} bids={bids} />
+                        <ProductActions isLive={true} currency={currency} buyAmount={price} bidAmount={price} />
+                    </Grid>
+                </Grid>
+        </div>
+    );
+}
