2f504f1e3b5874a0a8351e0d6455f2757523df19
Borislava <bobiivanova00@gmail.com>
2021-11-29 15:39:16 +0200

    add text class

--- src/components/description/Description.jsx
+++ src/components/description/Description.jsx
@@ -5,7 +5,7 @@ import styles from "./Description.module.scss";
 export default function Description({ text , image }) {
     return(
         <div className={styles.description}>
-            <Typography variant="body1" className={image.text}>{text}</Typography>
+            <Typography variant="body1" className={styles.text}>{text}</Typography>
             <img src={image} className={styles.image} />
         </div>
     );
