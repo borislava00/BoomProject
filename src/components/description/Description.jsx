2289809142d9b45c6508a493a5faef0c7312936e
Borislava <bobiivanova00@gmail.com>
2021-11-29 15:30:12 +0200

    add Description component

---
+++ src/components/description/Description.jsx
@@ -0,0 +1,12 @@
+import Typography from "@mui/material/Typography";
+
+import styles from "./Description.module.scss";
+
+export default function Description({ text , image }) {
+    return(
+        <div className={styles.description}>
+            <Typography variant="body1" className={image.text}>{text}</Typography>
+            <img src={image} className={styles.image} />
+        </div>
+    );
+}
